0a465cd0b2f703a97299c0a2bd1b19b5dea7c906
madoke <madoke@gmail.com>
2017-10-10 15:15:08 +0100

    add a backend for blockchain service

--- bananachain-client/src/main/java/com/example/bananachainclient/BananachainClientUI.java
+++ bananachain-client/src/main/java/com/example/bananachainclient/BananachainClientUI.java
@@ -1,16 +1,21 @@
 package com.example.bananachainclient;
 
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.Label;
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 @Theme("BananachainClient")
 public class BananachainClientUI extends UI{
 	
 	@Override
 	protected void init(VaadinRequest request){
-		Label lbl = new Label("Hello vaadin");
-		setContent(lbl);
+		VerticalLayout layout = new VerticalLayout();
+		layout.setSpacing(false);
+		layout.addComponent(new Button("Component 1"));
+		layout.addComponent(new Button("Component 2"));
+		layout.addComponent(new Button("Component 3"));
+		setContent(layout);
 	}
 }
---
+++ bananachain-client/src/main/java/com/example/bananachainclient/BananachainService.java
@@ -0,0 +1,19 @@
+package com.example.bananachainclient;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class BananachainService {
+
+    private static BananachainService instance;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BananachainService.class.getName());
+
+
+    public List<String> getBlocks() {
+        return null;
+    }
+
+
+}
